eae3d807ba7e885ade166498c080cdf880316fa7
akshathaa22 <akshathaajagannathan@gmail.com>
2026-05-11 18:21:24 +0530

    1st commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+	resource_providers {
+		azure_rm = {
+			source= "hashicorp/azurerm"
+			version= "~3.0"
+		}
+	}
+}
+
+provider "azurerm" {
+	features {}
+}
+
+resource "azurerm_resource_group" "demo" {
+	name= "rg_devops_demo"
+	location= "Central India"
+}
