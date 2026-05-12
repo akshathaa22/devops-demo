e3ccfed1ba2040ffc2bc46b00310f769e8598ea6
akshathaa22 <akshathaajagannathan@gmail.com>
2026-05-12 19:58:38 +0530

    trying to commit2

--- main.tf
+++ main.tf
@@ -15,3 +15,11 @@ resource "azurerm_resource_group" "demo" {
 	name= "rg_devops_demo"
 	location= "Central India"
 }
+
+resource "azurerm_storage_account" "demo" {
+	name                     = "akshustr12345"
+	resource_group_name      = azurerm_resource_group.demo.name
+	location                 = azurerm_resource_group.demo.location
+	account_tier             = "Standard"
+	account_replication_type = "LRS"
+}
