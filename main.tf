48fe425305ed3259fdd34d6aed8645708312c08a
akshathaa22 <akshathaajagannathan@gmail.com>
2026-06-30 17:49:13 +0530

    testing3

--- main.tf
+++ main.tf
@@ -1,25 +1,25 @@
 terraform {
-	required_providers {
-		azurerm = {
-			source= "hashicorp/azurerm"
-			version= "~> 3.0"
-		}
-	}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
 }
 
 provider "azurerm" {
-	features {}
+  features {}
 }
 
 resource "azurerm_resource_group" "demo" {
-	name= "rg_devops_demo"
-	location= "Central India"
+  name     = "rg_devops_demo"
+  location = "Central India"
 }
 
 resource "azurerm_storage_account" "demo" {
-	name                     = "akshustr12345"
-	resource_group_name      = azurerm_resource_group.demo.name
-	location                 = azurerm_resource_group.demo.location
-	account_tier             = "Standard"
-	account_replication_type = "LRS"
+  name                     = "akshustr12345"
+  resource_group_name      = azurerm_resource_group.demo.name
+  location                 = azurerm_resource_group.demo.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
